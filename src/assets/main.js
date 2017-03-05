bb954d3d04e223615f652e4863a08e534a2b9b9d
Ednas <edna@edna.tech>
2017-03-05 14:37:55 -0500

    Started on setHiddenFields function

--- src/assets/main.js
+++ src/assets/main.js
@@ -7,3 +7,22 @@ function guess() {
 }
 
 //implement new functions here
+
+function setHiddenFields() {
+	let attempt = '0';
+	let number = Math.floor(Math.random()*10000);
+	console.log(number);
+}
+setHiddenFields();
+
+
+
+// Hint: In order to add a zero to the front of an answer, it must be a string, not a number. You can convert numbers to strings with .toString(). We can create a while loop that runs while answer.length is less than 4 that puts a 0 before answer's current value.
+
+// Set the hidden input attempt's value to zero
+// In our setHiddenFields function we should also set the hidden input attempt to 0.
+
+// Only set the answer and attempt hidden inputs when they aren't already set
+// Call the setHiddenFields function in the body of the guess function, but also write some logic so that it's only called when answer and attempt haven't already been set.
+
+// Hint: we can use an if condition to only run our code when answer or attempt is empty ('').
